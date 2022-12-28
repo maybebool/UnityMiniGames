ce260bca44a81ac43f94ea1b9546412162f29f48
Roman Aebi <unity2.sae@gmail.com>
2022-12-28 22:13:11 +0100

    Pipes Destroyer

---
+++ Assets/FlappyBird/Scripts/Pipes.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+namespace FlappyBird.Scripts
+{
+    public class Pipes : MonoBehaviour
+    {
+        public float speed = 5f;
+        private float leftEdge;
+
+        private void Start()
+        {
+            leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1;
+        }
+
+
+        private void Update()
+        {
+            transform.position += Vector3.left * (Time.deltaTime * speed);
+
+            if (transform.position.x < leftEdge)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/FlappyBird/Scripts/Spawner.cs
+++ Assets/FlappyBird/Scripts/Spawner.cs
@@ -1,9 +1,31 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace FlappyBird.Scripts
 {
     public class Spawner : MonoBehaviour
     {
-        
+        public GameObject prefab;
+        public float spawnRate = 1;
+        public float minHight = -1;
+        public float maxHeight = 1f;
+
+        private void OnEnable()
+        {
+            InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+        }
+
+
+        private void OnDisable()
+        {
+            CancelInvoke(nameof(Spawn));
+        }
+
+        private void Spawn()
+        {
+            var pipes = Instantiate(prefab, transform.position, Quaternion.identity);
+            pipes.transform.position += Vector3.up * Random.Range(minHight, maxHeight);
+        }
     }
 }
