4a7f33f49a672bc4275c9744804be711c8f31c94
Roman Aebi <unity2.sae@gmail.com>
2023-03-06 15:13:23 +0100

    follow script for Background

---
+++ Assets/DoodleJump/Scripts/Destroy.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+namespace DoodleJump.Scripts {
+	public class Destroy : MonoBehaviour {
+		[SerializeField] private GameObject player;
+		[SerializeField] private GameObject platform;
+		[SerializeField] private GameObject spring;
+		private GameObject myPlat;
+
+
+		// Start is called before the first frame update
+		void Start() {
+		}
+
+		// Update is called once per frame
+		void Update() {
+		}
+
+		private void OnTriggerEnter2D(Collider2D col) {
+			myPlat = Instantiate(Random.Range(1, 15) > 1 ? platform : spring, 
+				new Vector2(Random.Range(-5.5f, 5.5f), 
+					player.transform.position.y + (2 + Random.Range(0.5f, 1f))), Quaternion.identity);
+
+			Destroy(col.gameObject);
+		}
+	}
+}
--- Assets/DoodleJump/Scripts/PlayerController.cs
+++ Assets/DoodleJump/Scripts/PlayerController.cs
@@ -1,29 +1,46 @@
+using System;
+using GlobalScripts;
 using UnityEngine;
+using UnityEngine.Animations;
 
 namespace DoodleJump.Scripts
 {
     public class PlayerController : MonoBehaviour
     {
 
-        private Rigidbody2D rb2d;
+        private Rigidbody2D _rb2d;
+        private float _moveInput;
+        private float _speed = 10;
+        private float _localX;
+        public GameName flag;
 
-        private float moveInput;
-
-        private float speed = 10;
-        
         // Start is called before the first frame update
         void Start()
         {
-            rb2d = GetComponent<Rigidbody2D>();
+            _rb2d = GetComponent<Rigidbody2D>();
+            _localX = transform.localScale.x;
+        }
 
+        private void Update() {
+            _moveInput = Input.GetAxis("Horizontal");
         }
 
         // Update is called once per frame
-        void FixedUpdate()
-        {
-            moveInput = Input.GetAxis("Horizontal");
-            rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y);
+        private void FixedUpdate() {
+            Flip();
+            _rb2d.velocity = new Vector2(_moveInput * _speed, _rb2d.velocity.y);
+        }
 
+
+        private void Flip() {
+            if (_moveInput > 0) {
+                gameObject.transform.localScale = new Vector3(_localX, transform.localScale.y, transform.localScale.z);
+            }
+
+            if (_moveInput < 0) {
+                gameObject.transform.localScale =
+                    new Vector3((-1) * _localX, transform.localScale.y, transform.localScale.z);
+            }
         }
     }
 }
