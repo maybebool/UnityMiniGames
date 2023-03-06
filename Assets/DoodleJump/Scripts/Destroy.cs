6b8f54166e9558e5376cc01fd82ecf44f8b1b089
Roman Aebi <unity2.sae@gmail.com>
2023-03-06 20:13:48 +0100

    pooling

--- Assets/DoodleJump/Scripts/Destroy.cs
+++ Assets/DoodleJump/Scripts/Destroy.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.Pool;
 using Random = UnityEngine.Random;
 
 
@@ -7,7 +9,21 @@ namespace DoodleJump.Scripts {
 		[SerializeField] private GameObject player;
 		[SerializeField] private GameObject platform;
 		[SerializeField] private GameObject spring;
+		// [SerializeField] private bool usePool;
+		// private ObjectPool<GameObject> _pool;
 		private GameObject myPlat;
+
+		// private void Start() {
+		// 	_pool = new ObjectPool<GameObject>(() => {
+		// 		return Instantiate(platform);
+		// 	}, o => {
+		// 		gameObject.SetActive(true);
+		// 	}, o => {
+		// 		gameObject.SetActive(false);
+		// 	}, o => {
+		// 		Destroy(this.gameObject);
+		// 	}, false, 10, 20);
+		// }
 
 
 		private void OnTriggerEnter2D(Collider2D col) {
@@ -18,7 +34,7 @@ namespace DoodleJump.Scripts {
 					
 					Destroy(col.gameObject);
 					Instantiate(spring,
-						new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (2 + Random.Range(0.2f, 1f))),
+						new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + ( Random.Range(0.2f, 1f))),
 						Quaternion.identity);
 				}
 				else {
@@ -34,10 +50,20 @@ namespace DoodleJump.Scripts {
 				else {
 					Destroy(col.gameObject);
 					Instantiate(platform,
-						new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (2 + Random.Range(0.2f, 1f))),
+						new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (Random.Range(0.2f, 1f))),
 						Quaternion.identity);
 				}
 			}
+		}
+
+
+		private void KillPlatform(GameObject platf){
+			// if (usePool) {
+			// 	_pool.Release(platf);
+			// }
+			// else {
+				Destroy(this.gameObject);
+			// }
 		}
 	}
 }
