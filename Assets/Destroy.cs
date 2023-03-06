82a58e83a877c6aaf4f7295095da7dc26a2b403a
Roman Aebi <unity2.sae@gmail.com>
2023-03-06 14:17:04 +0100

    spring jump

--- Assets/Destroy.cs
+++ Assets/Destroy.cs
@@ -6,26 +6,25 @@ using Random = UnityEngine.Random;
 
 
 public class Destroy : MonoBehaviour {
+	[SerializeField] private GameObject player;
+	[SerializeField] private GameObject platform;
+	[SerializeField] private GameObject spring;
+	private GameObject myPlat;
 
-    [SerializeField] private GameObject player;
-    [SerializeField] private GameObject platform;
-    private GameObject myPlat;
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start() {
+	}
 
-    private void OnTriggerEnter2D(Collider2D col) {
-        myPlat = Instantiate(platform, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (1 + Random.Range(0.5f, 1f))), Quaternion.identity);
-        Destroy(col.gameObject);
-    }
+	// Update is called once per frame
+	void Update() {
+	}
+
+	private void OnTriggerEnter2D(Collider2D col) {
+		myPlat = Instantiate(Random.Range(1, 15) > 1 ? platform : spring, 
+			new Vector2(Random.Range(-5.5f, 5.5f), 
+				player.transform.position.y + (1 + Random.Range(0.5f, 1f))), Quaternion.identity);
+
+		Destroy(col.gameObject);
+	}
 }
--- Assets/DoodleJump/Scripts/PlatformBounce.cs
+++ Assets/DoodleJump/Scripts/PlatformBounce.cs
@@ -1,10 +1,21 @@
 using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+
+
+[System.Flags]
+public enum PlatformType {
+    Nothing = 0, 
+    NormalBounce = 1,
+    SpringBounce = 2,
+    JetPackBounce = 4
+}
 
 namespace DoodleJump.Scripts
 {
-    public class PlatformBounce : MonoBehaviour
-    {
+    public class PlatformBounce : MonoBehaviour {
+
+        public PlatformType bounce;
         
         // Start is called before the first frame update
         void Start()
@@ -22,7 +33,17 @@ namespace DoodleJump.Scripts
         {
             if (col.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0)
             {
-                col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 600f);
+                if (bounce == PlatformType.Nothing) {
+                    return;
+                }
+
+                if (bounce == PlatformType.NormalBounce) {
+                    col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 600f);
+                }
+
+                if (bounce == PlatformType.SpringBounce) {
+                    col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 1200f);
+                }
             }
         }
     }
