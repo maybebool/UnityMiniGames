77f74287e34abbf05e485b3ca9316c3d0bd3f75d
Roman Aebi <unity2.sae@gmail.com>
2023-05-22 14:50:32 +0200

    ghost tile

--- Assets/Tetris/Scripts/GhostPiece.cs
+++ Assets/Tetris/Scripts/GhostPiece.cs
@@ -7,16 +7,16 @@ namespace Tetris.Scripts
     public class GhostPiece : MonoBehaviour
     {
         public Tile tile;
-        public Board board;
+        public Board mainBoard;
         public Piece trackingPiece;
-        
-        public Tilemap tileMap { get; private set; }
+
+        public Tilemap tilemap { get; private set; }
         public Vector3Int[] cells { get; private set; }
         public Vector3Int position { get; private set; }
 
         private void Awake()
         {
-            tileMap = GetComponentInChildren<Tilemap>();
+            tilemap = GetComponentInChildren<Tilemap>();
             cells = new Vector3Int[4];
         }
 
@@ -25,33 +25,54 @@ namespace Tetris.Scripts
             Clear();
             Copy();
             Drop();
-            
+            Set();
         }
-
 
         private void Clear()
         {
-            for (int i = 0; i < cells.Length; i++) {
-                var tilePos = cells[i] + position;
-                tileMap.SetTile(tilePos, null);
+            for (int i = 0; i < cells.Length; i++)
+            {
+                Vector3Int tilePosition = cells[i] + position;
+                tilemap.SetTile(tilePosition, null);
             }
         }
 
-        private void Drop()
+        private void Copy()
         {
-            
-        }
-
-        private void Copy() {
             for (int i = 0; i < cells.Length; i++) {
                 cells[i] = trackingPiece.Cells[i];
             }
         }
 
-        private void Set() {
-            for (int i = 0; i < cells.Length; i++) {
-                var tilePos = cells[i] + position;
-                tileMap.SetTile(tilePos, tile);
+        private void Drop()
+        {
+            Vector3Int position = trackingPiece.Position;
+
+            int current = position.y;
+            int bottom = -mainBoard.boardSize.y / 2 - 1;
+
+            mainBoard.Clear(trackingPiece);
+
+            for (int row = current; row >= bottom; row--)
+            {
+                position.y = row;
+
+                if (mainBoard.IsValidPosition(trackingPiece, position)) {
+                    this.position = position;
+                } else {
+                    break;
+                }
+            }
+
+            mainBoard.Set(trackingPiece);
+        }
+
+        private void Set()
+        {
+            for (int i = 0; i < cells.Length; i++)
+            {
+                Vector3Int tilePosition = cells[i] + position;
+                tilemap.SetTile(tilePosition, tile);
             }
         }
         
