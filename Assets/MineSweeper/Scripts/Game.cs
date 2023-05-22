3a0bd6fe84ac7ffc91564d5a8a625efa46a491d5
Roman Aebi <unity2.sae@gmail.com>
2023-05-22 18:53:03 +0200

    MIne counter

--- Assets/MineSweeper/Scripts/Game.cs
+++ Assets/MineSweeper/Scripts/Game.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 namespace MineSweeper.Scripts
 {
@@ -7,6 +9,7 @@ namespace MineSweeper.Scripts
     {
         public int width = 16;
         public int height = 16;
+        public int mineCount = 32;
 
         private MS_Board _board;
         private Cell[,] _state;
@@ -23,10 +26,11 @@ namespace MineSweeper.Scripts
             NewGame();
         }
 
-        private void NewGame()
-        {
+        private void NewGame() {
             _state = new Cell[width, height];
             GenerateCells();
+            GenerateMines();
+            GenerateNumbers();
             _camera.transform.position = new Vector3(width / 2f, height / 2f, -10f);
             _board.Draw(_state);
         }
@@ -43,6 +47,75 @@ namespace MineSweeper.Scripts
                     _state[x, y] = cell;
                 }
             }
+        }
+
+        private void GenerateMines()
+        {
+            for (int i = 0; i < mineCount; i++)
+            {
+                var x = Random.Range(0, width);
+                var y = Random.Range(0, height);
+
+                while (_state[x,y].type == Cell.Type.Mine) {
+                    
+                    x++;
+                    if (x >= width) {
+                        x = 0;
+                        y++;
+                        if (y >= height) {
+                            y = 0;
+                        }
+                    }
+                }
+                _state[x, y].type = Cell.Type.Mine;
+                //_state[x, y].revealed = true;
+            }
+        }
+
+        private void GenerateNumbers() {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    var cell = _state[x, y];
+                    if (cell.type == Cell.Type.Mine) {
+                        continue;
+                    }
+
+                    cell.number = CountMines(x, y);
+                    if (cell.number > 0) {
+                        cell.type = Cell.Type.Number;
+                    }
+
+                    cell.revealed = true;
+                    _state[x, y] = cell;
+                }
+            }
+        }
+
+        private int CountMines(int cellX, int cellY)
+        {
+            var count = 0;
+            for (int adjX = -1; adjX <= 1; adjX++) {
+                for (int adjY = -1; adjY <= 1; adjY++) {
+
+                    if (adjX == 0 && adjY == 0) {
+                        continue;
+                    }
+                    var x = cellX + adjX;
+                    var y = cellY + adjY;
+
+                    if (x < 0 || x >= width || y < 0 || y >= height)
+                    {
+                        continue;
+                    }
+
+                    if (_state[x,y].type == Cell.Type.Mine)
+                    {
+                        count++;
+                    }
+                }
+            }
+            
+            return count;
         }
     }
 }
