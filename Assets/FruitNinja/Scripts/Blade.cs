3ff9a9f9e27b69df4bdd34330e4f287282fca386
Roman Aebi <unity2.sae@gmail.com>
2023-06-26 10:51:39 +0200

    slice method

--- Assets/FruitNinja/Scripts/Blade.cs
+++ Assets/FruitNinja/Scripts/Blade.cs
@@ -1,8 +1,31 @@
+using System;
 using UnityEngine;
 
 namespace FruitNinja.Scripts {
-    public class Blade : MonoBehaviour
-    {
-    
+    public class Blade : MonoBehaviour {
+        private bool sclicing;
+        
+        private void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                StartSlicing();
+            }else if (Input.GetMouseButtonUp(0)) {
+                StopSlicing();
+            }else if (sclicing) {
+                ContinueSlicing();
+            }
+        }
+
+        private void StartSlicing() {
+            sclicing = true;
+        }
+
+        private void StopSlicing() {
+            sclicing = false;
+        }
+
+        private void ContinueSlicing() {
+            
+        }
+        
     }
 }
