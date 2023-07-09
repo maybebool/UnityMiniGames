2e2cf701077e4203b103b59560500c194423f69d
Roman Aebi <unity2.sae@gmail.com>
2023-07-09 05:12:35 +0200

    bomb logic

---
+++ Assets/FruitNinja/Scripts/Bomb.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace FruitNinja.Scripts {
+    public class Bomb : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other) {
+            if (other.CompareTag("Player")) {
+                FindObjectOfType<FruitNinjaGameManager>().Explode();
+            }
+        }
+    }
+}
--- Assets/FruitNinja/Scripts/FruitNinjaGameManager.cs
+++ Assets/FruitNinja/Scripts/FruitNinjaGameManager.cs
@@ -9,7 +9,14 @@ namespace FruitNinja.Scripts {
         public TMP_Text scoreText;
 
         private int _score;
+        private Blade _blade;
+        private Spawner _spawner;
 
+
+        private void Awake() {
+            _blade = FindObjectOfType<Blade>();
+            _spawner = FindObjectOfType<Spawner>();
+        }
 
         private void Start() {
             NewGame();
@@ -23,6 +30,11 @@ namespace FruitNinja.Scripts {
         public void IncreaseScore(int amount) {
             _score += amount;
             scoreText.text = _score.ToString();
+        }
+
+        public void Explode() {
+            _blade.enabled = false;
+            _spawner.enabled = false;
         }
         
 
