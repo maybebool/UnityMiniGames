f869cd83c4a3ee2cc14e738f5b15c8564351f58d
Roman Aebi <unity2.sae@gmail.com>
2023-07-23 12:29:30 +0200

    FlappyBird optimizaation

--- Assets/FlappyBird/Scripts/Pipes.cs
+++ Assets/FlappyBird/Scripts/Pipes.cs
@@ -1,27 +1,28 @@
-using System;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace FlappyBird.Scripts
 {
     public class Pipes : MonoBehaviour
     {
-        public float speed = 5f;
-        private float leftEdge;
-
-        private void Start()
-        {
-            leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 12;
+        [SerializeField] private float speed = 4f;
+        [SerializeField] private float xOffset = 10f;
+        [SerializeField] private float yOffset;
+        [SerializeField] private bool isFirstPipe;
+        
+        private void Start() {
+            if (isFirstPipe) return;
+            var rnd = Random.Range(-yOffset, yOffset);
+            transform.position = new Vector3(transform.position.x, rnd, 0);
         }
 
-
-        private void Update()
-        {
+        private void Update() {
+            
             transform.position += Vector3.left * (Time.deltaTime * speed);
-
-            if (transform.position.x < leftEdge)
-            {
-                Destroy(gameObject);
+            
+            if (transform.position.x <= -xOffset) {
+                var rnd = Random.Range(-yOffset, yOffset);
+                transform.position = new Vector3(xOffset, rnd, 0);
             }
         }
     }
