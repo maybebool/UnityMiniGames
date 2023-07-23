61ca5b896542b858762bc3576f0f171d40482001
Roman Aebi <unity2.sae@gmail.com>
2023-07-23 11:25:17 +0200

    Asteroid scene setup

---
+++ Assets/Asteroids/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Asteroids.Scripts {
+    public class AsteroidSpawner : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
