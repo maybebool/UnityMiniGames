f7d6c84d6b695fca51ee2f10b489ab634df83531
Roman Aebi <unity2.sae@gmail.com>
2023-09-01 18:41:14 +0200

    stuff

--- Assets/GlobalScripts/Parallax.cs
+++ Assets/GlobalScripts/Parallax.cs
@@ -1,38 +1,35 @@
 using System;
 using UnityEngine;
 
-namespace GlobalScripts
-{
+namespace GlobalScripts {
     [Flags]
     public enum GameName {
         Nothing = 0,
         FlappyBird = 1,
         DoodleJump = 2
     }
+
     public class Parallax : MonoBehaviour {
         public GameName gameName;
         private MeshRenderer _meshRenderer;
         public float animationSpeed = 0.05f;
 
 
-
-        private void Awake()
-        {
+        private void Awake() {
             _meshRenderer = GetComponent<MeshRenderer>();
-            
         }
 
-        private void Update()
-        {
+        private void Update() {
             if (gameName == GameName.Nothing) {
                 return;
             }
+
             if (gameName == GameName.FlappyBird) {
                 _meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
             }
 
             if (gameName == GameName.DoodleJump) {
-                _meshRenderer.material.mainTextureOffset += new Vector2( 0, animationSpeed * Time.deltaTime);
+                _meshRenderer.material.mainTextureOffset += new Vector2(0, animationSpeed * Time.deltaTime);
             }
         }
     }
