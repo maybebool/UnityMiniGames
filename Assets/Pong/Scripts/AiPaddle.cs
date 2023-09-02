9dda62269fcb812085b6d5e49684a0c2d9b983a8
Roman Aebi <unity2.sae@gmail.com>
2023-09-02 10:55:58 +0200

    formatting

--- Assets/Pong/Scripts/AiPaddle.cs
+++ Assets/Pong/Scripts/AiPaddle.cs
@@ -1,30 +1,25 @@
-
 using System;
 using UnityEngine;
 
-namespace Pong.Scripts
-{
-    public class AiPaddle : Paddle
-    {
+namespace Pong.Scripts {
+    public class AiPaddle : Paddle {
         public Rigidbody2D ball;
         public float threshold = 2;
-        
-        private void FixedUpdate()
-        {
-            if (ball.velocity.x > 0 + threshold){
 
-                if (ball.position.y > transform.position.y)
-                {
+        private void FixedUpdate() {
+            if (ball.velocity.x > 0 + threshold) {
+                if (ball.position.y > transform.position.y) {
                     Rigidbody.AddForce(Vector2.up * speed);
-                }else if (ball.position.y < transform.position.y) {
+                }
+                else if (ball.position.y < transform.position.y) {
                     Rigidbody.AddForce(Vector2.down * speed);
                 }
             }
-            else
-            {
+            else {
                 if (transform.position.y > 0.0f) {
                     Rigidbody.AddForce(Vector2.down * speed);
-                }else if (transform.position.y < 0.0f) {
+                }
+                else if (transform.position.y < 0.0f) {
                     Rigidbody.AddForce(Vector2.up * speed);
                 }
             }
