e3f59071856d55b3dfd6a2fb7607d1f9ec9537d4
Roman Aebi <unity2.sae@gmail.com>
2023-09-06 19:11:51 +0200

    formatting

--- Assets/FlappyBird/Scripts/Player.cs
+++ Assets/FlappyBird/Scripts/Player.cs
@@ -2,10 +2,8 @@ using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-namespace FlappyBird.Scripts
-{
-    public class Player : MonoBehaviour
-    {
+namespace FlappyBird.Scripts {
+    public class Player : MonoBehaviour {
         private Vector3 _direction;
         [SerializeField] private float gravity = -9.8f;
         [SerializeField] private float strength = 5f;
@@ -13,30 +11,24 @@ namespace FlappyBird.Scripts
         [SerializeField] private Sprite[] sprites;
         private int spriteIndex;
         [SerializeField] private GameObject manager;
-        
 
-        private void Awake()
-        {
+
+        private void Awake() {
             spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
-        private void Start()
-        {
-            InvokeRepeating(nameof(AnimateSprite),0.15f, 0.15f);
+        private void Start() {
+            InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
         }
 
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-            {
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
                 _direction = Vector3.up * strength;
             }
 
-            if (Input.touchCount > 0)
-            {
+            if (Input.touchCount > 0) {
                 Touch touch = Input.GetTouch(0);
-                if (touch.phase == TouchPhase.Began)
-                {
+                if (touch.phase == TouchPhase.Began) {
                     _direction = Vector3.up * strength;
                 }
             }
@@ -45,26 +37,21 @@ namespace FlappyBird.Scripts
             transform.position += _direction * Time.deltaTime;
         }
 
-        private void AnimateSprite()
-        {
+        private void AnimateSprite() {
             spriteIndex++;
-            if (spriteIndex >= sprites.Length)
-            {
+            if (spriteIndex >= sprites.Length) {
                 spriteIndex = 0;
             }
 
             spriteRenderer.sprite = sprites[spriteIndex];
         }
 
-        private void OnTriggerEnter2D(Collider2D col)
-        {
-            if (col.gameObject.CompareTag("Obsticle"))   
-            {
+        private void OnTriggerEnter2D(Collider2D col) {
+            if (col.gameObject.CompareTag("Obsticle")) {
                 //FindObjectOfType<GameManager>().GameOver();
                 manager.GetComponent<GameManager>().GameOver();
-                
-            }else if (col.gameObject.CompareTag("Scoring"))
-            {
+            }
+            else if (col.gameObject.CompareTag("Scoring")) {
                 manager.GetComponent<GameManager>().IncreaseScore();
             }
         }
