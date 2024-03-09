155474da2785df5a07ddf883d033d32056ed64c7
Roman Aebi <unity2.sae@gmail.com>
2024-03-09 15:55:39 +0100

    GameManger script + menu scene

---
+++ Assets/Menu/Scenes.cs
@@ -0,0 +1,18 @@
+﻿namespace Menu {
+    public enum Scenes {
+        Asteroids = 0, 
+        Breakout = 1,
+        BubbleShooter = 2,
+        DoodleJump = 3,
+        FlappyBird = 4,
+        FruitNinja = 5,
+        MineSweeper = 6,
+        Pong = 7,
+        Snake = 8,
+        SpaceInvaders = 9,
+        SpaceWars = 10,
+        Tetris = 11,
+        TicTacToe = 12,
+        Wimmelbild = 13
+    }
+}
