da4d8af2134843c178c54f089b1d0f7cba0c69b7
Roman Aebi <unity2.sae@gmail.com>
2024-03-10 14:44:46 +0100

    fixing problems in Bubble Shooter

---
+++ Assets/BubbleShooter/Scripts/BubbleList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BubbleShooter.Scripts {
+    public static class BubbleList {
+        
+        public static List<GameObject> bubbleList = new();
+    }
+}
